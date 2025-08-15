7ce02cd6e35297920d32753d321854ae8bba115f
szwanne <103660814+szwanne@users.noreply.github.com>
2025-08-15 19:08:54 +0200

    Initial commit

---
+++ trip/src/components/Popular.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Popular() {
+  const getPopular = async () => {};
+  return <div>Popular</div>;
+}
+
+export default Popular;
---
+++ trip/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Popular from "../components/Popular";
+import Activities from "../components/Activities";
+
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <Popular />
+      <Activities />
+    </div>
+  );
+}
+
+export default Home;
