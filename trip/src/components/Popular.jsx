951a33ea7e111f56f430e179721ad1b0e923b272
szwanne <103660814+szwanne@users.noreply.github.com>
2025-08-15 20:24:18 +0200

    Fetch data from Rapidapi in Popular.jsx file

--- trip/src/components/Popular.jsx
+++ trip/src/components/Popular.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function Popular() {
-  const getPopular = async () => {};
+  useEffect(() => {
+    getPopular();
+  }, []);
+
+  const getPopular = async () => {
+    try {
+      const response = await fetch(
+        "https://travel-advisor.p.rapidapi.com/attractions/list-in-boundary?bl_latitude=34.0522&tr_latitude=34.0522&bl_longitude=-118.2437&tr_longitude=-118.2437&limit=9&currency=USD&lunit=km&lang=en_US",
+        {
+          method: "GET",
+          headers: {
+            "X-RapidAPI-Key": import.meta.env.VITE_API_KEY,
+            "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
   return <div>Popular</div>;
 }
 
